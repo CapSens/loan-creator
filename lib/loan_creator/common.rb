bef8a75a7ac623d230f0367e7545c51a2d3d2e2f
Nicolas Besnard <besnard.nicolas@gmail.com>
2016-11-09 16:20:48 +0100

    working spec

--- lib/loan_creator/common.rb
+++ lib/loan_creator/common.rb
@@ -1,17 +1,18 @@
+require 'date'
+
 module LoanCreator
   class Common
-
     def initialize(amount_in_cents:, annual_interests_rate:,
       starts_at:, duration_in_months:, deferred_in_months: 0)
-      @amount_in_cents = amount_in_cents
+      @amount_in_cents       = amount_in_cents
       @annual_interests_rate = annual_interests_rate
-      @starts_at = starts_at
-      @duration_in_months = duration_in_months
-      @deferred_in_months = deferred_in_months
+      @starts_at             = starts_at
+      @duration_in_months    = duration_in_months
+      @deferred_in_months    = deferred_in_months
     end
 
-    def self.end_date
-      @starts_at.next_month(@duration_in_months)
+    def end_date
+      Date.parse(@starts_at).next_month(@duration_in_months)
     end
   end
 end
--- spec/loan_creator/common_spec.rb
+++ spec/loan_creator/common_spec.rb
@@ -3,9 +3,14 @@ require "spec_helper"
 describe LoanCreator::Common do
   describe '.end_date' do
     it 'should give the end date of the loan' do
-      # new_loan = LoanCreator::Common.send(initialize())
-      new_loan = LoanCreator::Common.send(initialize(100000, 10, '2016-01-15', 4))
-      expect(new_loan.end_date).to eq('2016-05-15')
+      new_loan = LoanCreator::Common.new(
+        amount_in_cents:       100000,
+        annual_interests_rate: 10,
+        starts_at:             '2016-01-15',
+        duration_in_months:    4
+      )
+
+      expect(new_loan.end_date).to eq(Date.parse('2016-05-15'))
     end
   end
 end
