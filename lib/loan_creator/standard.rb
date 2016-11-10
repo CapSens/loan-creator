bc5130e3c9c5cb9c1a2310b55208d9ab95d8d126
thibaulth <thibault@capsens.eu>
2016-11-10 14:29:38 +0100

    start standard loan

--- lib/loan_creator/standard.rb
+++ lib/loan_creator/standard.rb
@@ -1,7 +1,66 @@
 module LoanCreator
   class Standard < LoanCreator::Common
     def time_table
+      time_table = []
+      calc_remaining_capital = self.amount_in_cents
+      calc_paid_interests = 0
 
+      self.duration_in_months.times do |term|
+        calc_monthly_interests = calc_remaining_capital *
+          (self.annual_interests_rate / 100.0) / 12.0
+        calc_monthly_capital = self.calc_monthly_payment -
+          calc_monthly_interests
+        calc_remaining_capital -= calc_monthly_capital
+        calc_paid_capital = self.amount_in_cents - calc_remaining_capital
+        calc_paid_interests += calc_monthly_interests
+        calc_remaining_int = self.total_interests - calc_paid_interests
+
+
+        time_table << LoanCreator::TimeTable.new(
+          term:                            term + 1,
+          monthly_payment:                 self.calc_monthly_payment,
+          monthly_payment_capital_share:   calc_monthly_capital,
+          monthly_payment_interests_share: calc_monthly_interests,
+          remaining_capital:               calc_remaining_capital,
+          paid_capital:                    calc_paid_capital,
+          remaining_interests:             calc_paid_interests,
+          paid_interests:                  calc_remaining_int
+        )
+      end
+
+      time_table
+    end
+
+    def calc_monthly_payment
+      @calc_monthly_payment ||= _calc_monthly_payment
+    end
+
+    def total_interests
+      @total_interests ||= _total_interests
+    end
+
+    def interests_difference
+      (self.amount_in_cents + total_interests -
+        (self.duration_in_months *
+        (self.amount_in_cents *
+        ((self.annual_interests_rate / 100.0) / 12.0) / (1 -
+        ((1 + ((self.annual_interests_rate / 100.0) / 12.0)) **
+        ((-1) * self.duration_in_months)))))).round
+    end
+
+    private
+
+    def _calc_monthly_payment
+      monthly_interests_rate = (self.annual_interests_rate / 100.0) / 12.0
+      denominator = (1 - ((1 + monthly_interests_rate) **
+        ((-1) * self.duration_in_months)))
+
+      (self.amount_in_cents * monthly_interests_rate / denominator).round
+    end
+
+    def _total_interests
+      (self.duration_in_months * self.calc_monthly_payment -
+        self.amount_in_cents).round
     end
   end
 end
--- spec/loan_creator/standard_spec.rb
+++ spec/loan_creator/standard_spec.rb
@@ -1,21 +1,92 @@
 require "spec_helper"
 
 describe LoanCreator::Standard do
+  describe "#time_table" do
 
-  describe '.time_table' do
-    # let(:input) {
-    #   {
-    #     amount_in_cents: 100000,
-    #     annual_interests_rate: 10,
-    #     starts_at: Date.new(2016, 12, 15),
-    #     ends_at: Date.new(2020, 11, 15)
-    #   }
-    # }
-    #
-    # let(:output) { subject.time_table(input) }
-    #
-    # it "" do
-    #   expect(output).to eq()
+    # The loan
+    subject(:loan) {
+      described_class.new(
+        amount_in_cents:       amount_in_cents,
+        annual_interests_rate: 10,
+        starts_at:             '2016-01-15',
+        duration_in_months:    duration_in_months
+      )
+    }
+
+    # Duration of the loan
+    let(:duration_in_months) { 24 }
+
+    # Loan amount
+    let(:amount_in_cents) { 100_000 * 100 }
+
+    # Loan monthly payment calculation's result
+    let(:monthly_payment) { subject.calc_monthly_payment }
+
+    # Loan total interests calculation's result
+    let(:total_interests) { subject.total_interests }
+
+    # Time tables array (full loan)
+    let(:time_tables) { subject.time_table }
+
+    it "returns 'duration_in_months' elements" do
+      expect(time_tables.size).to eql(duration_in_months)
+    end
+
+    it 'has the same equal monthly payment on each term' do
+      all_tt = time_tables.all? { |tt|
+        tt.monthly_payment == monthly_payment }
+      expect(all_tt).to eql(true)
+    end
+
+    it "has a difference in cents between 'total_interests'
+        and the sum of the rounded 'monthly_interests'" do
+       expect(subject.interests_difference).to eql(-6)
+    end
+
+    # it 'has an incresaing amount of paid interests' do
+    #   pass = true
+    #   all_except_last_term.each_with_index do |tt, i|
+    #     unless tt.paid_interests == (i + 1) * monthly_interests
+    #       pass = false
+    #     end
+    #   end
+    #   expect(pass).to eql(true)
     # end
+    #
+    # it 'has a decreasing amount of remaining interests' do
+    #   pass = true
+    #   all_except_last_term.each_with_index do |tt, i|
+    #     unless tt.remaining_interests ==
+    #            (total_interests - ((i + 1) * monthly_interests))
+    #       pass = false
+    #     end
+    #   end
+    #   expect(pass).to eql(true)
+    # end
+
+  end
+
+  describe "#total_interests" do
+    it "has the expected value - example one" do
+      total_interests = described_class.new(
+        amount_in_cents:       100_000 * 100,
+        annual_interests_rate: 10,
+        starts_at:             '2016-01-15',
+        duration_in_months:    24
+      ).total_interests
+
+      expect(total_interests).to eql(1_074_776)
+    end
+
+    it "has the expected value - example two" do
+      total_interests = described_class.new(
+        amount_in_cents:       350_456_459 * 100,
+        annual_interests_rate: 7.63,
+        starts_at:             '2016-01-15',
+        duration_in_months:    17
+      ).total_interests
+
+      expect(total_interests).to eql(2_039_377_012)
+    end
   end
 end
