3579f82094720c5da619f7c3da8c2553b00a83df
Jerome Drevet <jerome.drevet@protonmail.com>
2020-06-24 11:18:58 +0200

    add new loan type uncapitalized bullet

---
+++ lib/loan_creator/uncapitalized_bullet.rb
@@ -0,0 +1,34 @@
+module LoanCreator
+  class UncapitalizedBullet < LoanCreator::Common
+    def lender_timetable
+      raise ArgumentError.new(:deferred_in_periods) unless deferred_in_periods == 0
+      raise ArgumentError.new(:interests_start_date) unless interests_start_date.nil?
+      timetable = new_timetable
+      reset_current_term
+      @crd_beginning_of_period = amount
+      @crd_end_of_period = amount
+      (duration_in_periods - 1).times { timetable << current_term }
+      compute_last_term
+      timetable << current_term
+      timetable
+    end
+
+    private
+
+    def compute_last_term
+      @crd_end_of_period = bigd('0')
+      @period_interests = total_interests
+      @period_capital = @crd_beginning_of_period
+      @total_paid_capital_end_of_period = @period_capital
+      @total_paid_interests_end_of_period = @period_interests
+      @period_amount_to_pay = @period_capital + @period_interests
+    end
+
+    def total_interests
+      amount.mult(
+        bigd(periodic_interests_rate) * bigd(duration_in_periods),
+        BIG_DECIMAL_DIGITS
+      )
+    end
+  end
+end
