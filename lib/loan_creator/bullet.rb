c2821dfa239c25ad15510c8240d3393ecda4c178
Eliot BARDET <eliot@capsens.eu>
2020-11-12 17:54:08 +0100

    [1/capitalized-interests] refactor

--- lib/loan_creator/bullet.rb
+++ lib/loan_creator/bullet.rb
@@ -16,13 +16,13 @@ module LoanCreator
     private
 
     def compute_last_term
-      @crd_end_of_period = bigd('0')
-      @period_interests = total_interests
-      @period_capital = @crd_beginning_of_period
-      @total_paid_capital_end_of_period = @period_capital
+      @crd_end_of_period                  = bigd('0')
+      @period_interests                   = total_interests
+      @period_capital                     = @crd_beginning_of_period
+      @total_paid_capital_end_of_period   = @period_capital
       @total_paid_interests_end_of_period = @period_interests
-      @period_amount_to_pay = @period_capital + @period_interests
-      @capitalized_interests = compute_capitalized_interests(duration_in_periods)
+      @period_amount_to_pay               = @period_capital + @period_interests
+      @capitalized_interests              = compute_capitalized_interests(duration_in_periods)
     end
 
     def compute_capitalized_interests(period)
