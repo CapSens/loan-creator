81183cfcc1d31f9b48c22f15513f7665a5a46a00
MZiserman <martinziserman@gmail.com>
2021-03-24 12:57:11 +0100

    fix method name/reset due on & index

--- lib/loan_creator/common.rb
+++ lib/loan_creator/common.rb
@@ -202,12 +202,17 @@ module LoanCreator
     def compute_last_term
       @crd_end_of_period                  = bigd('0')
       @due_interests_beginning_of_period  = @due_interests_end_of_period
-      @period_interests                   = @due_interests_end_of_period + period_generated_interests
+      @period_interests                   = @due_interests_beginning_of_period + compute_period_generated_interests
       @due_interests_end_of_period        = 0
       @period_capital                     = @crd_beginning_of_period
       @total_paid_capital_end_of_period   += @period_capital
       @total_paid_interests_end_of_period += @period_interests
       @period_amount_to_pay               = @period_capital + @period_interests
+
+      # handled in Timetable.<<
+      @due_on = nil
+      @index ||= @starting_index
+      @index += 1
     end
   end
 end
