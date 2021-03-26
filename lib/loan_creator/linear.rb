a5a281c38d5d8823af8d0fbab3099a04b194354d
MZiserman <martinziserman@gmail.com>
2021-03-26 20:19:45 +0100

    handle due interests linear

--- lib/loan_creator/linear.rb
+++ lib/loan_creator/linear.rb
@@ -30,18 +30,10 @@ module LoanCreator
       @crd_beginning_of_period = @crd_end_of_period
       @due_interests_beginning_of_period = @due_interests_end_of_period
 
-      @period_theoric_interests = compute_period_generated_interests
+      @period_theoric_interests = period_theoric_interests
       @delta_interests = @period_theoric_interests - @period_theoric_interests.round(2)
       @accrued_delta_interests += @delta_interests
-      @amount_to_add = bigd(
-        if @accrued_delta_interests >= bigd('0.01')
-          '0.01'
-        elsif @accrued_delta_interests <= bigd('-0.01')
-          '-0.01'
-        else
-          '0'
-        end
-      )
+      @amount_to_add = bigd(@accrued_delta_interests.truncate(2))
       @accrued_delta_interests -= @amount_to_add
       @period_interests = @period_theoric_interests.round(2) + @amount_to_add
       @period_capital = period_capital
@@ -53,12 +45,35 @@ module LoanCreator
       @index = idx + 1
     end
 
+    def period_theoric_interests
+      if @deferred_period
+        compute_period_generated_interests
+      elsif @due_interests_beginning_of_period > 0
+        reimbursed_due_interests + compute_period_generated_interests
+      else
+        compute_period_generated_interests
+      end
+    end
+
     def period_capital
       if @deferred_period
         bigd(0)
+      elsif @due_interests_beginning_of_period > 0
+        compute_period_capital - reimbursed_due_interests
       else
-        (amount / (duration_in_periods - deferred_in_periods)).round(2)
+        compute_period_capital
       end
+    end
+
+    def reimbursed_due_interests
+      [
+        @due_interests_beginning_of_period,
+        compute_period_capital
+      ].min
+    end
+
+    def compute_period_capital
+      ((amount + @initial_due_interests) / (duration_in_periods - deferred_in_periods)).round(2)
     end
   end
 end
