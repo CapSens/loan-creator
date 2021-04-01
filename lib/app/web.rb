bd377d8fb4d43d7eb03199f3462b6b5f99969227
Juliette Audema <juliette@capsens.eu>
2021-04-01 09:08:12 +0200

    Add table

--- lib/app/web.rb
+++ lib/app/web.rb
@@ -1,18 +1,31 @@
 require "sinatra/base"
 require_relative "./helpers/application_helper"
 require 'pry'
+require_relative "../loan_creator"
 
 class Web < Sinatra::Base
   helpers ApplicationHelper
 
   get "/" do
-    if params[:initial_values].nil?
-      @params = fix_params_type(params.merge({initial_values:{}}))
-    else
-      @params =  fix_params_type(params)
-    end
+    @params = fix_params_type(params)
 
-    #binding.pry
-    erb :index, locals: { fixed_params: @params }
+    @timetable =  case params[:type]
+                  when 'in_fine'
+                    LoanCreator::InFine.new(**@params)
+                  when 'bullet'
+                    LoanCreator::Bullet.new(**@params)
+                  when 'linear'
+                    LoanCreator::Linear.new(**@params)
+                  when 'standard'
+                    LoanCreator::Standard.new(**@params)
+                  end
+
+    @terms =  if @timetable
+                @timetable.lender_timetable.terms
+              else
+                []
+              end
+
+    erb :index, locals: { fixed_params: @params, terms: @terms }
   end
 end
