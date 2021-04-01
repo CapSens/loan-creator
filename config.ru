391fb039f8e944d14954f8087c5436d8be9adc93
Juliette Audema <juliette@capsens.eu>
2021-04-01 09:24:39 +0200

    Add Web in module LoanCreator

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require_relative "./lib/app/web"
 
-run Web
+run LoanCreator::Web
--- lib/app/web.rb
+++ lib/app/web.rb
@@ -2,29 +2,31 @@ require "sinatra/base"
 require_relative "./helpers/application_helper"
 require_relative "../loan_creator"
 
-class Web < Sinatra::Base
-  helpers ApplicationHelper
+module LoanCreator
+  class Web < Sinatra::Base
+    helpers ApplicationHelper
 
-  get "/" do
-    @params = fix_params_type(params)
+    get "/" do
+      @params = fix_params_type(params)
 
-    @timetable =  case params[:type]
-                  when 'in_fine'
-                    LoanCreator::InFine.new(**@params)
-                  when 'bullet'
-                    LoanCreator::Bullet.new(**@params)
-                  when 'linear'
-                    LoanCreator::Linear.new(**@params)
-                  when 'standard'
-                    LoanCreator::Standard.new(**@params)
-                  end
+      @timetable =  case params[:type]
+                    when 'in_fine'
+                      LoanCreator::InFine.new(**@params)
+                    when 'bullet'
+                      LoanCreator::Bullet.new(**@params)
+                    when 'linear'
+                      LoanCreator::Linear.new(**@params)
+                    when 'standard'
+                      LoanCreator::Standard.new(**@params)
+                    end
 
-    @terms =  if @timetable
-                @timetable.lender_timetable.terms
-              else
-                []
-              end
+      @terms =  if @timetable
+                  @timetable.lender_timetable.terms
+                else
+                  []
+                end
 
-    erb :index, locals: { fixed_params: @params, terms: @terms }
+      erb :index, locals: { fixed_params: @params, terms: @terms }
+    end
   end
 end
