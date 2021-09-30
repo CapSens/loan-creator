38a27c047c9e2f6a5930a99863a362765fc5e83c
MZiserman <martinziserman@gmail.com>
2021-09-30 14:14:15 +0200

    rm useless arg, multi part bullet term

--- lib/loan_creator/bullet.rb
+++ lib/loan_creator/bullet.rb
@@ -24,7 +24,7 @@ module LoanCreator
     def compute_last_term(timetable)
       @crd_end_of_period                  = bigd('0')
       @due_interests_beginning_of_period  = @due_interests_end_of_period
-      @period_interests                   = @due_interests_end_of_period + compute_capitalized_interests(@due_on, timetable)
+      @period_interests                   = @due_interests_end_of_period + compute_capitalized_interests(timetable)
       @due_interests_end_of_period        = 0
       @period_capital                     = @crd_beginning_of_period
       @total_paid_capital_end_of_period   += @period_capital
@@ -32,14 +32,17 @@ module LoanCreator
       @period_amount_to_pay               = @period_capital + @period_interests
     end
 
-    def compute_capitalized_interests(due_date, timetable)
-      computed_periodic_interests_rate = periodic_interests_rate(timetable_term_dates[timetable.next_index - 1], due_date)
-      (amount + @due_interests_beginning_of_period).mult(computed_periodic_interests_rate, BIG_DECIMAL_DIGITS)
+    def compute_capitalized_interests(timetable)
+      if term_dates? && (timetable_term_dates[timetable.next_index - 1] + 1.year) < @due_on
+        multi_part_interests(timetable_term_dates[timetable.next_index - 1], @due_on)
+      else
+        compute_period_generated_interests(periodic_interests_rate(timetable_term_dates[timetable.next_index - 1], @due_on))
+      end
     end
 
     def compute_term(timetable)
       @due_interests_beginning_of_period = @due_interests_end_of_period
-      @due_interests_end_of_period += compute_capitalized_interests(@due_on, timetable)
+      @due_interests_end_of_period += compute_capitalized_interests(timetable)
     end
   end
 end
