8f470750c11b2234a8bc1721ffdcdabf1db7c110
MZiserman <martinziserman@gmail.com>
2021-09-30 18:19:32 +0200

    specs

--- spec/loan_creator/in_fine_spec.rb
+++ spec/loan_creator/in_fine_spec.rb
@@ -80,16 +80,25 @@ describe LoanCreator::InFine do
     context 'given a scenario with term dates' do
       term_dates = ['2018-10-15', '2020-10-15']
 
-      initial_values = {
-        paid_capital: 0.0,
-        paid_interests: 0.0,
-        accrued_delta_interests: 0.0,
-        starting_index: 2,
-        due_interests: 0.0
-      }
       scenario = ['year', '100000', '12', '2018-10-15', '1', '0']
 
-      include_examples('valid lender timetable', loan_type, scenario, initial_values, true, term_dates)
+      include_examples('valid lender timetable', loan_type, scenario, {}, true, term_dates)
+    end
+
+    context 'given a scenario with term dates' do
+      term_dates = ['2020-10-15', '2022-10-15']
+
+      scenario = ['year', '100000', '12', '2020-10-15', '1', '0']
+
+      include_examples('valid lender timetable', loan_type, scenario, {}, true, term_dates)
+    end
+
+    context 'given a scenario with term dates' do
+      term_dates = ['2018-10-15', '2021-10-15']
+
+      scenario = ['month', '100000', '12', '2018-10-15', '1', '0']
+
+      include_examples('valid lender timetable', loan_type, scenario, {}, true, term_dates)
     end
 
     context 'given a scenario with due_interests' do
